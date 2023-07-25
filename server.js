bb57090d6d07eb3dd0e8ed978135540da896cd13
Q ZHANG <64118871+castlehunter@users.noreply.github.com>
2023-07-25 11:52:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-/*********************************************************************************
- *  WEB422 – Assignment 1
- *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
- *  No part of this assignment has been copied manually or electronically from any other source
- *  (including web sites) or distributed to other students.
- * 
- *  Name: Qing Zhang     Student ID: 130982218     Date: 19-May-2023
- *  Cyclic Link: https://periwinkle-hare-boot.cyclic.app
- *
- ********************************************************************************/
-
-
 const express = require("express");
 const cors = require("cors");
 const env = require("dotenv").config();
